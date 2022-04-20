de93a1af70462dd01723caf46f00f2d7313e8737
MaksimAkimov2003 <a.t.n20034@gmail.com>
2022-04-20 18:15:36 +0700

    Add View Binding and intent

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) // hi beba
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            var intent = Intent(MainActivity@this, CodingActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }

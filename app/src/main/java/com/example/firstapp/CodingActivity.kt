52e2af9f52fc72db7cf70f2042e17fce19181834
MaksimAkimov2003 <a.t.n20034@gmail.com>
2022-04-22 18:46:02 +0700

    add second activity and icon

--- app/src/main/java/com/example/firstapp/CodingActivity.kt
+++ app/src/main/java/com/example/firstapp/CodingActivity.kt
@@ -2,10 +2,17 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.firstapp.databinding.ActivityCodingBinding
 
-class CodingActivity : AppCompatActivity() {
+class CodingActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityCodingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coding)
+        binding = ActivityCodingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }

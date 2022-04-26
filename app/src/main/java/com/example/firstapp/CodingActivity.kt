470c9da56c2abeefaccecf6608aff32051c82847
MaksimAkimov2003 <a.t.n20034@gmail.com>
2022-04-27 02:13:46 +0700

    drag and drop(very bad)

--- app/src/main/java/com/example/firstapp/CodingActivity.kt
+++ app/src/main/java/com/example/firstapp/CodingActivity.kt
@@ -1,11 +1,22 @@
 package com.example.firstapp
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.graphics.Canvas
+import android.graphics.Point
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.DragEvent
 import android.view.View
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.firstapp.databinding.ActivityCodingBinding
 
 class CodingActivity : AppCompatActivity(){
+    private val buttonVarDragMessage = "buttonVar Added"
+
     private lateinit var binding: ActivityCodingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +24,108 @@ class CodingActivity : AppCompatActivity(){
         binding = ActivityCodingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        attachViewDragListener()
 
+        binding.dropArea.setOnDragListener(AddDragListener)
+    }
+
+    private val AddDragListener = View.OnDragListener { view, dragEvent ->
+        val draggableItem = dragEvent.localState as View
+
+        when (dragEvent.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                true
+            }
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                binding.dropArea.alpha = 0.3f
+                true
+            }
+            DragEvent.ACTION_DRAG_LOCATION -> {
+                true
+            }
+            DragEvent.ACTION_DRAG_EXITED -> {
+                binding.dropArea.alpha = 1.0f
+                draggableItem.visibility = View.VISIBLE
+
+                view.invalidate()
+
+                true
+            }
+            DragEvent.ACTION_DROP -> {
+                binding.dropArea.alpha = 1.0f
+
+                if (dragEvent.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+                    val draggedData = dragEvent.clipData.getItemAt(0).text
+                    println("draggedData $draggedData")
+                }
+
+                draggableItem.x = dragEvent.x - (draggableItem.width / 2)
+                draggableItem.y = dragEvent.y - (draggableItem.height / 2)
+
+                val parent = draggableItem.parent as LinearLayout
+                parent.removeView(draggableItem)
+
+                val dropPlace = view as ConstraintLayout
+
+                dropPlace.addView(draggableItem)
+
+                true
+            }
+            DragEvent.ACTION_DRAG_ENDED -> {
+                draggableItem.visibility = View.VISIBLE
+                view.invalidate()
+
+                true
+            }
+            else -> {
+                false
+            }
+
+        }
+    }
+
+    private fun attachViewDragListener() {
+        binding.buttonVar.setOnLongClickListener { view: View ->
+
+            val item = ClipData.Item(buttonVarDragMessage)
+            val dataToDrag = ClipData(
+                buttonVarDragMessage,
+                arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                item
+            )
+
+            val maskShadow = MaskDragShadowBuilder(view)
+
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                @Suppress("DEPRECATION")
+                view.startDrag(dataToDrag, maskShadow, view, 0)
+            }
+
+            else {
+                view.startDragAndDrop(dataToDrag, maskShadow, view, 0)
+            }
+
+            view.visibility = View.INVISIBLE
+
+            true
+        }
     }
 }
+
+private class MaskDragShadowBuilder(view: View) : View.DragShadowBuilder(view) {
+    private val shadow = view
+
+    override fun onProvideShadowMetrics(size: Point?, touch: Point?) {
+        val width: Int = view.width
+        val height: Int = view.height
+
+        size?.set(width, height)
+        touch?.set(width / 2, height / 2)
+    }
+
+    override fun onDrawShadow(canvas: Canvas?) {
+        shadow.draw(canvas)
+    }
+}
+
+

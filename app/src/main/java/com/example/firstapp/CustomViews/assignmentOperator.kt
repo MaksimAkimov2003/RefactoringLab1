d787a4e0fd5ea7abdcfcaf78793b82a44dd4c4b0
Vattghern1 <andre.2233@mail.ru>
2022-05-07 16:06:27 +0700

    Custom Views changes

--- app/src/main/java/com/example/firstapp/CustomViews/assignmentOperator.kt
+++ app/src/main/java/com/example/firstapp/CustomViews/assignmentOperator.kt
@@ -1,33 +1,31 @@
-package com.example.firstapp
-
+package com.example.firstapp.CustomViews
 
 import android.content.Context
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import com.example.firstapp.databinding.ViewDeclareIntegerBinding
+import com.example.firstapp.databinding.ViewAssignmentOperatorBinding
 
-class CustomViewDeclareInteger @JvmOverloads constructor(
+class assignmentOperator @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 
-) : ConstraintLayout(context, attrs, defStyleAttr) {
+    ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private val binding = ViewDeclareIntegerBinding.inflate(LayoutInflater.from(context), this)
+    private val binding = ViewAssignmentOperatorBinding.inflate(LayoutInflater.from(context), this)
 
 
     init {
         selfBlockDelete()
+
     }
 
     private fun selfBlockDelete() {
         binding.deleteBlock.setOnClickListener {
 
 
+
         }
     }
-
 }
-
-
